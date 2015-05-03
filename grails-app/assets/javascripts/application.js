7a575fac8554ebd7782dd37a1d7b62a224e1500a
hugo <hugo.rn@me.com>
2015-05-03 11:05:23 -0300

    Equiparação Romulo Adicionando CSS e JS do SB-Admin-2

--- grails-app/assets/javascripts/application.js
+++ grails-app/assets/javascripts/application.js
@@ -9,3 +9,49 @@
 //= require bootstrap
 //= require_tree .
 //= require_self
+
+
+var agenda = {
+	init : function(){
+		$("#frmAgenda").submit(agenda.submit);
+		agenda.loadLista();
+	},
+	submit: function(e){
+		e.preventDefault();
+		$.ajax({
+			url:"/evolucao/agenda/save",
+			type: "POST",
+			contentType: "application/json",
+			dataType: "json",
+			data: $("#frmAgenda").serialize()
+		}).done(function(){
+			$("#frmAgenda")[0].reset();
+			agenda.loadLista();
+		});
+	},
+	loadLista: function(){
+		$.ajax({
+			url:"/evolucao/agenda/listAll",
+			type: "GET",
+			contentType: "application/json"
+		}).done(function(data){
+			$("#tblAgenda tbody").html("");
+			$.each(data,function(idx,agenda){
+				var html = "<tr data-id=\""+agenda.id+"\">";
+				html += "<td>"+agenda.nome+"</td>";
+				if(agenda.grupo){
+					html += "<td>"+agenda.grupo+"</td>";
+				}else{
+					html += "<td> - </td>";
+				}
+				html += "<td>&nbsp;</td>";
+				html += "</tr>";
+				$("#tblAgenda tbody").append(html);
+			});
+		});
+	}
+};
+
+$(document).ready(function(){
+	agenda.init();
+});
