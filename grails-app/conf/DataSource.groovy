0dd845c2f8ff70b549953d9a6085f58c993293ce
leehernandiz <leehernandiz@gmail.com>
2015-06-01 21:00:42 -0300

    Atualização configuração Heroku Banco Produção

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,20 +1,14 @@
 dataSource {
     pooled = true
-    jmxExport = true
-    //ntlhugo
-    //driverClassName = "org.h2.Driver"
+    pooled = true
     driverClassName = "org.postgresql.Driver"
-    dialect = "org.hibernate.dialect.PostgreSQLDialect"
-    username = "qtfpyfktrpdjkg"
-    password = "pcVWN5RJ2xgmMNd65PcEE6Cqmu"
-    //ntlhugo
-    //username = "sa"
-    //password = ""
+    username = "eowjfwqmdejjbl"
+    password = "x_chokJwJj636JbPFFv1a5jDon"
+    dbCreate = "create-drop"
 }
 hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = false
-
 //    cache.region.factory_class = 'org.hibernate.cache.SingletonEhCacheRegionFactory' // Hibernate 3
     cache.region.factory_class = 'org.hibernate.cache.ehcache.SingletonEhCacheRegionFactory' // Hibernate 4
     singleSession = true // configure OSIV singleSession mode
@@ -25,45 +19,44 @@ hibernate {
 environments {
     development {
         dataSource {
-            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
-            //ntlhugo
-            //url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+            dbCreate = "update" // one of 'create', 'create-drop','update'
             url = "jdbc:postgresql://localhost:5432/unicar"
+            driverClassName = "org.postgresql.Driver"
+            username = "postgres"
+            password = "unirn"
         }
     }
     test {
         dataSource {
-            dbCreate = "update"
+            dbCreate = "update" // one of 'create', 'create-drop','update'
             url = "jdbc:postgresql://localhost:5432/unicar"
-            //ntlhugo
-            //url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
         }
     }
     production {
         dataSource {
-            dbCreate = "update"
-            //ntlhugo
-            //url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
-            url = "jdbc:postgresql://ec2-54-204-35-248.compute-1.amazonaws.com:5432/d6t074j1h6ag85"
+            dbCreate = "update" // one of 'create', 'create-drop','update'
+            url = "jdbc:postgresql://ec2-54-83-41-183.compute-1.amazonaws.com:5432/d5qqh87miborlf"
+            username ="eowjfwqmdejjbl"
+            password ="x_chokJwJj636JbPFFv1a5jDon"
             properties {
-               // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
-               jmxEnabled = true
-               initialSize = 5
-               maxActive = 50
-               minIdle = 5
-               maxIdle = 25
-               maxWait = 10000
-               maxAge = 10 * 60000
-               timeBetweenEvictionRunsMillis = 5000
-               minEvictableIdleTimeMillis = 60000
-               validationQuery = "SELECT 1"
-               validationQueryTimeout = 3
-               validationInterval = 15000
-               testOnBorrow = true
-               testWhileIdle = true
-               testOnReturn = false
-               jdbcInterceptors = "ConnectionState"
-               defaultTransactionIsolation = java.sql.Connection.TRANSACTION_READ_COMMITTED
+                // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
+                jmxEnabled = true
+                initialSize = 5
+                maxActive = 50
+                minIdle = 5
+                maxIdle = 25
+                maxWait = 10000
+                maxAge = 10 * 60000
+                timeBetweenEvictionRunsMillis = 5000
+                minEvictableIdleTimeMillis = 60000
+                validationQuery = "SELECT 1"
+                validationQueryTimeout = 3
+                validationInterval = 15000
+                testOnBorrow = true
+                testWhileIdle = true
+                testOnReturn = false
+                jdbcInterceptors = "ConnectionState"
+                defaultTransactionIsolation = java.sql.Connection.TRANSACTION_READ_COMMITTED
             }
         }
     }
